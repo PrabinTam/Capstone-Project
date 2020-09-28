a49c5f80e1dec539db536444e652ea8afb4e2c8f
PrabinTam <48957755+PrabinTam@users.noreply.github.com>
2020-09-27 22:58:57 -0400

    Update sensitive_info_exe.py

--- sensitive_info_exe.py
+++ sensitive_info_exe.py
@@ -2,21 +2,27 @@ from capstone.file_detection_exe import initiate_watchdog
 import os
 import time
 
-def luhn_algorithm(path):
-    credit_card = []
-with open("test.txt", "r") as f:
-    for line in f:
-        if any(map(str.isdigit, line)) == True:         # only if the line consists of a number
-            line = line.strip()
-            line = line.replace("\n", "").replace("  ", " ")
-            line = line.split(" ")
-            for digit in line:
-                if digit.digit():             # if all the strings are all num
-                    if len(digit) == 15 or len(digit) == 16:    # if the string legth is equal to 15 or 16
-                        credit_card.append(digit)
+# The scritp here will go through all the files [txt, csv, docs and xls] in the system as long as
+# And check if they contain any personal information. 
 
+
+# This checks if the file has any potential credit card number.
+def credit_card_check(path):
+    credit_card = []
+    with open("test.txt", "r") as f:
+        for line in f:
+            if any(map(str.isdigit, line)) == True:         # only if the line consists of a number
+                line = line.strip()
+                line = line.replace("\n", "").replace("  ", " ")
+                line = line.split(" ")
+                for digit in line:
+                    if digit.digit():             # if all the strings are all num
+                        if len(digit) == 15 or len(digit) == 16:    # if the string legth is equal to 15 or 16
+                            credit_card.append(digit)
+    return credit_card
+    
 def detect_sensitive_file(path):
-    luhn_algorithm(path)
+    credit_card_check(path)
 
 class Handler(PatternMatchingEventHandler):
     def __init__(self):
