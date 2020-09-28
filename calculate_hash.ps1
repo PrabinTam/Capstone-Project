9bdb5f3fb6972d2cf7ecea16a585f8e560cc7839
PrabinTam <48957755+PrabinTam@users.noreply.github.com>
2020-09-27 22:53:20 -0400

    Update calculate_hash.ps1

--- calculate_hash.ps1
+++ calculate_hash.ps1
@@ -1,4 +1,5 @@
-﻿foreach ($file in (Get-Content .\hash_path.txt))
+﻿# uses powershell to get all the processes running in OS Windows. 
+ foreach ($file in (Get-Content .\hash_path.txt))
     {
         Get-FileHash $file -Algorithm sha256 | Select-Object -Property path,hash
        
